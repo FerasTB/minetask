733176f983e95307250d175ff0a7077ec42c60e4
FerasTB <al2mohtrf@gmail.com>
2023-04-28 11:06:26 +0300

    docs 2

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::post('/auth/register', [\App\Http\Controllers\Api\AuthController::class, 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::apiResource('doctor/info', App\Http\Controllers\Api\DoctorInfoController::class);
     Route::get('doctor/profile', [App\Http\Controllers\Api\DoctorInfoController::class, 'showMyInfo']);
-    Route::get('doctor/doctor/docs', [App\Http\Controllers\Api\DoctorInfoController::class, 'myRecords']);
+    Route::get('doctor/docs', [App\Http\Controllers\Api\DoctorInfoController::class, 'myRecords']);
     Route::get('doctor/patient/docs/{patient}', [App\Http\Controllers\Api\DoctorInfoController::class, 'myRecordsForPatient']);
     Route::get('doctor/case/docs/{case}', [App\Http\Controllers\Api\DoctorInfoController::class, 'myRecordsForCase']);
     Route::get('doctor/my_patient', [App\Http\Controllers\Api\DoctorInfoController::class, 'showMyPatient']);
