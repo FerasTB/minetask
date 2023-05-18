a1d2ba954aa53961ceb42b5b293f18f3e893efd7
FerasTB <al2mohtrf@gmail.com>
2023-05-18 20:21:52 +0300

    patient and supplier accounting profile 10

--- app/Http/Resources/AccountingProfileResource.php
+++ app/Http/Resources/AccountingProfileResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use App\Enums\OfficeType;
 use App\Models\Doctor;
 use App\Models\HasRole;
+use App\Models\Office;
 use App\Models\Patient;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -20,7 +21,7 @@ class AccountingProfileResource extends JsonResource
     {
         $patient = Patient::find($this->patient_id);
         $doctor = Doctor::find($this->doctor_id);
-        $office = Doctor::find($this->office_id);
+        $office = Office::find($this->office_id);
         if ($office->type == OfficeType::Separate) {
             $role = HasRole::where(['roleable_id' => $patient->id, 'roleable_type' => 'App\Models\Patient', 'user_id' => auth()->id()])->first();
         } else {
