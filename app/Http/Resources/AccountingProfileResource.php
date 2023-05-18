d4f7b878f0246c25f274dc059a8c39352b87f38e
FerasTB <al2mohtrf@gmail.com>
2023-05-18 20:52:01 +0300

    patient and supplier accounting profile 15

--- app/Http/Resources/AccountingProfileResource.php
+++ app/Http/Resources/AccountingProfileResource.php
@@ -22,7 +22,7 @@ class AccountingProfileResource extends JsonResource
         $doctor = Doctor::find($this->doctor_id);
         $office = Office::find($this->office_id);
         if ($office->type == OfficeType::Separate) {
-            $role = HasRole::where(['roleable_id' => $this->patient_id, 'roleable_type' => 'App\Models\Patient', 'user_id' => auth()->id()])->first();
+            $role = HasRole::where(['roleable_id' => $this->patient_id, 'roleable_type' => 'App\Models\Patient', 'user_id' => auth()->id()])->get();
         } else {
             $role = HasRole::where(['roleable_id' => $this->patient_id, 'roleable_type' => 'App\Models\Patient', 'user_id' => $office->owner->user_id])->first();
         }
