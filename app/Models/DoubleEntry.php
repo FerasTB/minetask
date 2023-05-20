44f2e1f6669df990b77f760f31acd9e8267dc855
FerasTB <al2mohtrf@gmail.com>
2023-05-20 15:23:52 +0300

    supplier 5

--- app/Models/DoubleEntry.php
+++ app/Models/DoubleEntry.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class DoubleEntry extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['COA_id', 'invoice_id', 'invoice_item_id', 'receipt_id', 'total_price', 'type'];
+
+    public function COA()
+    {
+        return $this->belongsTo(COA::class, 'COA_id');
+    }
 }
