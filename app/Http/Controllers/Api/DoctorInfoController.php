368b18b90a1fe8b76ff3f69a10028b1406e9c005
FerasTB <al2mohtrf@gmail.com>
2023-06-12 11:41:37 +0300

    drugs 5

--- app/Http/Controllers/Api/DoctorInfoController.php
+++ app/Http/Controllers/Api/DoctorInfoController.php
@@ -96,7 +96,7 @@ class DoctorInfoController extends Controller
 
     public function drug()
     {
-        return auth()->user()->doctor->drugs;
+        return auth()->user()->doctor->drugs->diagnosis;
     }
 
     public function activePatient(Office $office)
