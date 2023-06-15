314a1b42de2138cda47ca285e00d4fbf91e55175
FerasTB <al2mohtrf@gmail.com>
2023-06-15 13:01:07 +0300

    appointment 13

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -48,7 +48,7 @@ class AppointmentController extends Controller
                 'record',
             ])
             ->get();
-        $role = auth()->user()->roles->where('roleable_id', 1)->where('roleable_type', 'App\Models\Patient');
+        $role = auth()->user()->roles->where('roleable_id', 1)->where('roleable_type', 'App\Models\Patient')->first();
         return $role;
         return AppointmentResource::collection($appointments);
     }
