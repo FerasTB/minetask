cff89a145c577d3d7110404b0b1db78b51a7e526
FerasTB <al2mohtrf@gmail.com>
2023-06-24 16:41:44 +0300

    my patient 3

--- app/Http/Resources/MyPatientSeparateThroughAccountingProfileResource.php
+++ app/Http/Resources/MyPatientSeparateThroughAccountingProfileResource.php
@@ -20,36 +20,38 @@ class MyPatientSeparateThroughAccountingProfileResource extends JsonResource
     {
         // $role = HasRole::where(['roleable_id' => $this->patient_id, 'roleable_type' => 'App\Models\Patient', 'user_id' => auth()->id()])->first();
         $role = auth()->user()->roles->where('roleable_id', $this->patient_id)->where('roleable_type', 'App\Models\Patient')->first();
-        if ($role->sub_role == DoctorRoleForPatient::DoctorWithApprove) {
-            $patient = $this->patient;
-            return [
-                'id' => $patient->id,
-                'first_name' => $patient->first_name,
-                'last_name' => $patient->last_name,
-                'phone' => 0 . $patient->phone,
-                'email' => $patient->email,
-                'birth_date' => $patient->birth_date,
-                'note' => $patient->note,
-                'gender' => $patient->gender,
-                'status' => 'Approve'
-            ];
-        }
-        if ($role->sub_role == DoctorRoleForPatient::DoctorWithoutApprove) {
-            // $patient = TemporaryInformation::where(['patient_id' => $role->roleable_id, 'doctor_id' => auth()->user()->doctor->id])->first();
-            $patient = $this->temporaries->where('patient_id', $this->id)->where('doctor_id', auth()->user()->doctor->id)->first();
-            $originalPatient = $this->patient;
-            return [
-                'id' => $originalPatient->id,
-                'first_name' => $patient->first_name,
-                'last_name' => $patient->last_name,
-                'phone' => 0 . $originalPatient->phone,
-                'email' => $patient->email,
-                'birth_date' => $patient->birth_date,
-                'gender' => $originalPatient->gender,
-                'note' => $patient->note,
-                'status' => 'WithoutApprove',
-                'TemporaryId' => $patient->id,
-            ];
+        if ($role) {
+            if ($role->sub_role == DoctorRoleForPatient::DoctorWithApprove) {
+                $patient = $this->patient;
+                return [
+                    'id' => $patient->id,
+                    'first_name' => $patient->first_name,
+                    'last_name' => $patient->last_name,
+                    'phone' => 0 . $patient->phone,
+                    'email' => $patient->email,
+                    'birth_date' => $patient->birth_date,
+                    'note' => $patient->note,
+                    'gender' => $patient->gender,
+                    'status' => 'Approve'
+                ];
+            }
+            if ($role->sub_role == DoctorRoleForPatient::DoctorWithoutApprove) {
+                // $patient = TemporaryInformation::where(['patient_id' => $role->roleable_id, 'doctor_id' => auth()->user()->doctor->id])->first();
+                $patient = $this->temporaries->where('patient_id', $this->id)->where('doctor_id', auth()->user()->doctor->id)->first();
+                $originalPatient = $this->patient;
+                return [
+                    'id' => $originalPatient->id,
+                    'first_name' => $patient->first_name,
+                    'last_name' => $patient->last_name,
+                    'phone' => 0 . $originalPatient->phone,
+                    'email' => $patient->email,
+                    'birth_date' => $patient->birth_date,
+                    'gender' => $originalPatient->gender,
+                    'note' => $patient->note,
+                    'status' => 'WithoutApprove',
+                    'TemporaryId' => $patient->id,
+                ];
+            }
         }
         return [];
     }
