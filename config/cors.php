a0687b5f7ad9a05634f38dca99081e7644fa06d0
FerasTB <al2mohtrf@gmail.com>
2023-08-05 13:23:36 +0300

    cors 6

--- config/cors.php
+++ config/cors.php
@@ -15,15 +15,31 @@ return [
     |
     */
 
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    // 'allowed_methods' => ['*', 'PUT'],
+
+    // 'allowed_origins' => ['https://*marstaan.com', 'https://*.marstaan.com', 'https://clinic.marstaan.com', 'https://*.marstaan.com/*', 'http://127.0.0.1:5173', 'https://clinic.marstaan.com/*', 'https://boring-payne.212-227-188-105.plesk.page', 'https://boring-payne.212-227-188-105.plesk.page/*', 'https://clinic.marstaan.com/auth/login'],
+    // // 'allowed_origins' => ['*'],
+
+    // // 'allowed_origins_patterns' => ['/(*.marstaan)\.com'],
+    // 'allowed_origins_patterns' => ['*'],
+
+    // 'allowed_headers' => ['*'],
+
+    // 'exposed_headers' => [],
+
+    // 'max_age' => 0,
+
+    // 'supports_credentials' => true,
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*', 'PUT'],
+    'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://*marstaan.com', 'https://*.marstaan.com', 'https://clinic.marstaan.com', 'https://*.marstaan.com/*', 'http://127.0.0.1:5173', 'https://clinic.marstaan.com/*', 'https://boring-payne.212-227-188-105.plesk.page', 'https://boring-payne.212-227-188-105.plesk.page/*', 'https://clinic.marstaan.com/auth/login'],
-    // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['*'],
 
-    // 'allowed_origins_patterns' => ['/(*.marstaan)\.com'],
-    'allowed_origins_patterns' => ['*'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
@@ -31,6 +47,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
