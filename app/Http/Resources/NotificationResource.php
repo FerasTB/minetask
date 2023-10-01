d919dee18caa02af0282d9f5f57f0755a5a98bad
FerasTB <al2mohtrf@gmail.com>
2023-10-01 04:29:52 +0300

    mark as read 2

--- app/Http/Resources/NotificationResource.php
+++ app/Http/Resources/NotificationResource.php
@@ -15,6 +15,7 @@ class NotificationResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'data' => $this->data,
             'created_at' => $this->created_at,
         ];
