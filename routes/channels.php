a7517c05d41334a120e6a6b0c16da13696b40946
FerasTB <al2mohtrf@gmail.com>
2023-11-08 16:36:53 +0300

    pusher 7

--- routes/channels.php
+++ routes/channels.php
@@ -19,7 +19,7 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 });
 
 Broadcast::channel('App.Models.Doctor.{id}', function ($user, $id) {
-    return true;
+    return (int) $user->doctor->id === (int) $id;
 });
 
 Broadcast::channel('App.Models.DentalLab.{id}', function ($user, $id) {
