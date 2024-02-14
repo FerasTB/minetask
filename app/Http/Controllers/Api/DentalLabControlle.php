aefaa34e62a04e7923c8deaa5c024a3da6d99ce7
FerasTB <al2mohtrf@gmail.com>
2024-02-15 02:17:05 +0300

    secondary_initial_balance 2

--- app/Http/Controllers/Api/DentalLabControlle.php
+++ app/Http/Controllers/Api/DentalLabControlle.php
@@ -36,7 +36,7 @@ class DentalLabControlle extends Controller
             'office_id' => $office->id,
             'doctor_id' => auth()->user()->doctor->id,
             'COA_id' => $request->coa_id,
-            'initial_balance' => $request->initial_balance != null ? $request->initial_balance : 0,
+            'secondary_initial_balance' => $request->secondary_initial_balance != null ? $request->secondary_initial_balance : 0,
             'note' => $request->note,
             'type' => AccountingProfileType::DentalLabDoctorAccount,
         ]);
