9ab7fd805c0fc7f4660ac9f8376191d606108ee1
FerasTB <al2mohtrf@gmail.com>
2024-07-23 15:35:27 +0300

    coa total 6

--- app/Http/Resources/DirectDoubleEntryResource.php
+++ app/Http/Resources/DirectDoubleEntryResource.php
@@ -20,7 +20,7 @@ class DirectDoubleEntryResource extends JsonResource
             'total_price' => $this->total_price,
             'created_at' => $this->created_at,
             'running_balance' => $this->running_balance,
-            'type' => DoubleEntryType::getKey($this->type),
+            'type' => DoubleEntryType::getValue($this->type),
             'entryType' => 'DirectEntry',
             'coa' => new COAResource($this->whenLoaded('coa')),
         ];
