53d6426db64fb6badb9ba3fe6e3cb51414a5f350
FerasTB <al2mohtrf@gmail.com>
2024-08-11 19:52:26 +0300

    patient prefix 2

--- database/migrations/2024_08_11_160712_add_patient_id_to_user_infos_table.php
+++ database/migrations/2024_08_11_160712_add_patient_id_to_user_infos_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('user_infos', function (Blueprint $table) {
-            $table->foreignId('patient_id')->nullable()->constrained('Patients')->onDelete('cascade');
+            $table->foreignId('patient_id')->nullable()->constrained('Patients')->onDelete('set null');
         });
     }
 
